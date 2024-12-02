c99d7617149fc8d8bb0b4355ca7782523ccd0e6a
shafriki <shafriki.cse@gmail.com>
2024-12-02 17:36:47 +0600

    coffee added

--- index.js
+++ index.js
@@ -21,6 +21,25 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // coffee collection 
+    const coffeeCollection = client.db('coffeeDB').collection('coffee')
+
+    // read operation
+    app.get('/coffee', async(req, res) => {
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    // create operation
+    app.post('/coffee', async(req, res) =>{
+        const newCoffee = req.body;
+        console.log(newCoffee);
+        const result = await coffeeCollection.insertOne(newCoffee);
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -36,9 +55,13 @@ run().catch(console.dir);
 app.use(cors());
 app.use(express.json());
 
+
 app.get('/',(req, res) => {
     res.send('coffee server is running');
 })
+
+
+
 
 app.listen(port, ()=>{
     console.log(`running port: ${port}`);
